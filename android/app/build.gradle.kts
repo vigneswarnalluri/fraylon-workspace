01f639b889be454b9daf08c82e318f9f1cffea05
fraylontechnologies-boop <fraylontechnologies@gmail.com>
2026-07-08 19:23:14 +0530

    refactor: rename package to com.fraylon.workspace

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.fraylon.fraylon_workspace"
+    namespace = "com.fraylon.workspace"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "28.2.13676358"
 
@@ -17,7 +17,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.fraylon.fraylon_workspace"
+        applicationId = "com.fraylon.workspace"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/fraylon/workspace/MainActivity.kt
+++ android/app/src/main/kotlin/com/fraylon/workspace/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.fraylon.fraylon_workspace
+package com.fraylon.workspace
 
 import io.flutter.embedding.android.FlutterActivity
 
